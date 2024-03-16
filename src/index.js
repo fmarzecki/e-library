3264ad59ec6a1481774ee5c1489c766cf6b4e6b7
fmarzecki <filipo085@icloud.com>
2024-03-16 22:50:07 +0100

    Responsive Interface Added

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ProfilesPage from './components/test/ProfilesPage';
 import NotFoundPage from './components/error/NotFoundPage';
 import { Box, Container, CssBaseline } from '@mui/material';
 import Clicker from './components/test/Clicker';
+import Dashboard from './components/Dashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -23,21 +22,14 @@ const router = createBrowserRouter([
     element: <ProfilesPage/>
   },
   {
-    path: '/clicker',
-    element: <Clicker />,
+    path: '/lib',
+    element: <Dashboard />,
   }
 ]);
 
 root.render(
   <React.StrictMode>
     <CssBaseline/>
-    <Container >
       <RouterProvider router={router} />
-    </Container>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
