28dff252fc82c3fe5cd4d68b83e6149b78034c2f
MikolajKnap <mikozxc@gmail.com>
2024-03-16 11:34:46 +0100

    Czyszczenie plikow

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ProfilesPage from './components/test/ProfilesPage';
 import NotFoundPage from './components/error/NotFoundPage';
@@ -37,9 +35,3 @@ root.render(
     </Container>
   </React.StrictMode>
 );
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
